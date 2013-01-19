c3ef53e4dbf4f5e2c8db7a784fa23b4c9ba2c025
Rhys Powell <rhys@rpowell.me>
2013-01-19 17:19:21 +1100

    Added documentation for RPSyntaxTheme

--- RPSyntaxHighlighter/RPSyntaxTheme.h
+++ RPSyntaxHighlighter/RPSyntaxTheme.h
@@ -10,8 +10,41 @@
 
 @interface RPSyntaxTheme : NSObject
 
+/**
+ Loads a theme definition from a JSON file
+ 
+ This method loads a theme definition from a properly formatted JSON file and returns
+ a newly instantiated object from it.
+ 
+ @param filename the name of the JSON file, sans extension
+ 
+ @return A newly instantiated `RPSyntaxTheme`
+ */
 - (instancetype)initWithContentsOfFile:(NSString *)filename;
+
+/**
+ Style parts of an attributed string for a given style
+ 
+ You shouldn't need to call this method yourself. It applies styles as defined by
+ the theme over the ranges given to it, typically the results of an `RPSyntaxMatcher`
+ processing code.
+ 
+ @see RPSyntaxMatcher
+ 
+ @param string the string styles should be applied to
+ @param ranges an `NSSet` of `NSValue` objects containing the ranges to be styled
+ @param styleName a name corresponding to style which should be used to highlight the ranges
+ */
 - (void)styleString:(NSMutableAttributedString *)string atRanges:(NSSet *)ranges forStyle:(NSString *)styleName;
+
+/**
+ Default styles for the theme
+ 
+ You probably won't need to call this function. It provides the default style to use
+ for the given theme including font, foreground and background colours, and so on.
+ 
+ @return an NSDictionary of `NSAttributedString` styles
+ */
 - (NSDictionary *)defaultStyles;
 
 @end
